351c672360183198f708d575fc95cddf2a7ed513
Matt Ford <mat84@btinternet.com>
2015-10-16 19:42:28 +0100

    Fixed webview

--- app/src/main/java/uk/org/mattford/scoutlink/activity/RulesActivity.java
+++ app/src/main/java/uk/org/mattford/scoutlink/activity/RulesActivity.java
@@ -3,6 +3,7 @@ package uk.org.mattford.scoutlink.activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import uk.org.mattford.scoutlink.R;
 
@@ -12,6 +13,7 @@ public class RulesActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rules);
         WebView webview = (WebView)findViewById(R.id.webView);
+        webview.setWebViewClient(new WebViewClient());
         webview.loadUrl("http://www.scoutlink.net/get-connected/rules");
     }
 }
