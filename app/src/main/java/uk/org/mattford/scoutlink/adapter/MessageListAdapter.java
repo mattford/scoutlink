ec9e6edd9b8961876c14514e6fff08c069b16b8d
Matt Ford <mat84@btinternet.com>
2020-08-06 18:22:48 +0100

    Reduce notification importance Add date divider

--- app/src/main/java/uk/org/mattford/scoutlink/adapter/MessageListAdapter.java
+++ app/src/main/java/uk/org/mattford/scoutlink/adapter/MessageListAdapter.java
@@ -39,7 +39,21 @@ public class MessageListAdapter extends RecyclerView.Adapter<MessageListAdapter.
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Context context = holder.mView.getContext();
+        Message previousMessage = null;
+        if (position > 0) {
+            previousMessage = this.messages.get(position - 1);
+        }
         Message message = this.messages.get(position);
+
+        holder.mDivider.setVisibility(View.GONE);
+        if (previousMessage == null ||
+                message.getTimestamp().getTime() - previousMessage.getTimestamp().getTime() > 24 * 60 * 60 * 1000
+        ) {
+            holder.mDivider.setVisibility(View.VISIBLE);
+            DateFormat dateFormat = android.text.format.DateFormat.getDateFormat(context);
+            String dateString = dateFormat.format(message.getTimestamp());
+            holder.mDate.setText(dateString);
+        }
         TextView defaultTextView = new TextView(context);
         if (message.isType(Message.TYPE_ERROR)) {
             holder.mMessageView.setTextColor(context.getResources().getColor(R.color.red));
@@ -77,16 +91,15 @@ public class MessageListAdapter extends RecyclerView.Adapter<MessageListAdapter.
             context.getResources().getDisplayMetrics()
         );
         Drawable background = context.getResources().getDrawable(R.drawable.rounded_corners);
-        RecyclerView.LayoutParams layoutParams = (RecyclerView.LayoutParams)holder.mView.getLayoutParams();
+        LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams)holder.mMessageContainer.getLayoutParams();
         if (message.isSenderType(Message.SENDER_TYPE_SELF)) {
-            holder.mView.setGravity(Gravity.END);
-            background.setColorFilter(holder.mView.getResources().getColor(R.color.outgoing_message_background), PorterDuff.Mode.SRC_IN);
+            holder.mMessageContainer.setGravity(Gravity.END);
+            background.setColorFilter(context.getResources().getColor(R.color.outgoing_message_background), PorterDuff.Mode.SRC_IN);
         }
         layoutParams.leftMargin = message.isSenderType(Message.SENDER_TYPE_SELF) ? marginPx : defaultMarginPx;
         layoutParams.rightMargin = message.isSenderType(Message.SENDER_TYPE_OTHER) ? marginPx : defaultMarginPx;
         holder.mMessageView.setGravity(message.isSenderType(Message.SENDER_TYPE_SERVER) ? Gravity.CENTER : Gravity.START);
-        holder.mView.setBackgroundDrawable(background);
-        holder.mView.setLayoutParams(layoutParams);
+        holder.mMessageContainer.setBackground(background);
 
         if (message.getTimestamp() != null) {
             DateFormat dateFormat = android.text.format.DateFormat.getTimeFormat(context);
@@ -103,13 +116,19 @@ public class MessageListAdapter extends RecyclerView.Adapter<MessageListAdapter.
         final TextView mSenderView;
         final TextView mTimestampView;
         final TextView mMessageView;
+        final LinearLayout mDivider;
+        final LinearLayout mMessageContainer;
+        final TextView mDate;
 
         ViewHolder(View view) {
             super(view);
             mView = (LinearLayout) view;
             mSenderView = view.findViewById(R.id.sender);
             mTimestampView = view.findViewById(R.id.timestamp);
+            mMessageContainer = view.findViewById(R.id.message_container);
             mMessageView = view.findViewById(R.id.message);
+            mDivider = view.findViewById(R.id.date_divider);
+            mDate = view.findViewById(R.id.date);
         }
     }
 }
--- app/src/main/java/uk/org/mattford/scoutlink/irc/IRCService.java
+++ app/src/main/java/uk/org/mattford/scoutlink/irc/IRCService.java
@@ -262,7 +262,7 @@ public class IRCService extends Service {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             CharSequence name = "General";
             String description = "Connection status and new messages";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            int importance = NotificationManager.IMPORTANCE_LOW;
             NotificationChannel channel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, name, importance);
             channel.setDescription(description);
             // Register the channel with the system; you can't change the importance
