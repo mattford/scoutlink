a16b0ccbc0c08776a57f248c2af6e2f31589216a
Matt Ford <mford@grademaker.com>
2020-12-02 17:24:53 +0000

    simplify validation and usage hint for user defined alias

--- app/src/main/java/uk/org/mattford/scoutlink/command/handler/UserDefinedCommandHandler.java
+++ app/src/main/java/uk/org/mattford/scoutlink/command/handler/UserDefinedCommandHandler.java
@@ -3,6 +3,9 @@ package uk.org.mattford.scoutlink.command.handler;
 import android.content.Context;
 import android.os.Handler;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
 import java.util.StringJoiner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -23,23 +26,8 @@ public class UserDefinedCommandHandler extends CommandHandler {
     }
 
     public boolean validate(String[] params, Conversation conversation) {
-        for (String command : commands) {
-            Pattern p = Pattern.compile(argPattern);
-            Matcher m = p.matcher(command);
-            while (m.find()) {
-                // Groups: 1 - param number, 2 - "greedy" dash
-                try {
-                    int paramNumber = Integer.parseInt(m.group(1));
-                    if (paramNumber < 0 || paramNumber > params.length - 1) {
-                        return false;
-                    }
-                } catch (NumberFormatException e) {
-                    // Failed.
-                    return false;
-                }
-            }
-        }
-        return true;
+        int expectedParameterCount = getExpectedParameterCount();
+        return expectedParameterCount <= params.length - 1;
     }
 
     @Override
@@ -63,12 +51,8 @@ public class UserDefinedCommandHandler extends CommandHandler {
                     int paramNumber = Integer.parseInt(m.group(1));
                     String paramValue = "";
                     if (paramNumber >= 0 && params.length > paramNumber) {
-                        StringJoiner sj = new StringJoiner(" ");
                         if ("-".equalsIgnoreCase(m.group(2))) {
-                            for (int i = paramNumber; i < params.length; i++) {
-                                sj.add(params[i]);
-                            }
-                            paramValue = sj.toString();
+                            paramValue = StringUtils.join(Arrays.copyOfRange(params, paramNumber, params.length));
                         } else {
                             paramValue = params[paramNumber];
                         }
@@ -84,10 +68,30 @@ public class UserDefinedCommandHandler extends CommandHandler {
         }
     }
 
+    private int getExpectedParameterCount() {
+        int count = 0;
+        for (String command : commands) {
+            Pattern p = Pattern.compile(argPattern);
+            Matcher m = p.matcher(command);
+            while (m.find()) {
+                // Groups: 1 - param number, 2 - "greedy" dash
+                try {
+                    int paramNumber = Integer.parseInt(m.group(1));
+                    if (paramNumber > count) {
+                        count = paramNumber;
+                    }
+                } catch (NumberFormatException e) {
+                    // Failed.
+                }
+            }
+        }
+        return count;
+    }
 
     @Override
     public String getUsage() {
-        return "Incorrect usage, refer to Settings -> Aliases for correct usage.";
+        int expectedParameterCount = getExpectedParameterCount();
+        return "Incorrect usage, expected " + expectedParameterCount + " parameters.";
     }
 
     @Override
